bbdbcd90c179e412c7e2b4158209d78199b3612b
Akash <akash@bwowser.com>
2022-11-26 17:00:27 -0500

    Add ability to inject globals

--- code/index.js
+++ code/index.js
@@ -8,18 +8,21 @@ const backTickTagFn = (segments, ...args) => {
     })
 }
 
-module.exports = (template,args) => {
-    
+module.exports = (template,args, globals={}) => {
+    if(typeof globals !== "object") throw new Error("`globals` argument must be of type `object|undefined`")
     const context = {
         bt__: backTickTagFn,
         bt_: backTickTagFn,
         bt: backTickTagFn,
         $args: args,
         args,
+        ...globals
     }
 
-
-    const withBackticks = Function('{$args,bt_}={}', 'return bt_`' + template + '`');
+    const globalNames = Object.keys(globals);
+    const withBackticks = Function(
+        `{ $args, bt_, ${globalNames.join(', ')}}={}`, // arg1
+        'return bt_`' + template + '`');
     return withBackticks(context)
     // withBackticks.bind(context);
 }
