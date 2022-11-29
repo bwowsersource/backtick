9d6b7693220ca6bdff93e623cf61a51a7adc5616
Akash <akash@bwowser.com>
2022-11-29 00:56:48 -0500

    Added custom stack trace for js errors in template

--- code/errors.js
+++ code/errors.js
@@ -12,17 +12,47 @@ class UnprocessableValue extends Error {
     }
 }
 
-class TemplateError extends Error {
+class GenericTemplateError extends Error {
     constructor(e) {
         super(`There are errors in template: ${e.message}`);
         this.err = e;
     }
 }
 
-function processedError(e) {
-    const knownErrors = [ConstReInit, UnprocessableValue]
-    if (knownErrors.some(knownErr => e instanceof knownErr)) return e;
-    return new TemplateError(e);
+class TemplateErrorWithTrace extends Error {
+    constructor([srcLinesArr, line, col, e]) {
+
+        const srcLines = ['...', ...srcLinesArr, '...'].join('\n');
+        let stack = e.message;
+        stack += ` at line:${line + 1}${col ? ' col:' + (col + 1) : ''}`;
+        stack += '\n' + srcLines + '\n';
+        super(e.message);
+        this.stack = stack;
+        this.message = e.message
+        this.err = { line, col, e };
+    }
 }
 
-module.exports = { ConstReInit, UnprocessableValue, TemplateError, processedError };
+
+function parseStack(e, sourceTemplate) {
+    const [_msg, firstLine] = e.stack.split('\n');
+    if (firstLine) {
+        const [all, line, colWithColon, colNum] = firstLine.match(new RegExp(/:([0-9]+)(:([0-9]+))?[^0-9]*$/)) || [];
+        const lineIndex = ((line - 1) - 2); //stack-line numer index starts from 1. line0 is "function()", line1 is opening "{"
+        if (!lineIndex) return new GenericTemplateError(e);
+        const srcLineArr = sourceTemplate.split('\n')
+            .map((line, index) => (index === lineIndex ? '>>>' : '') + '\t' + line)
+            .filter((line, index) => Math.abs(lineIndex - index) < 3);
+
+        const colIndex = (colNum || 1) - 1; // stack colNum index starts from 1
+        return new TemplateErrorWithTrace([srcLineArr, lineIndex, colIndex, e])
+    }
+}
+function processedError(e, sourceTemplate) {
+    const knownErrors = [ConstReInit, UnprocessableValue]
+    if (knownErrors.some(knownErr => e instanceof knownErr)) return e;
+    if (sourceTemplate) return parseStack(e, sourceTemplate);
+    return new GenericTemplateError(e);
+}
+
+module.exports = { ConstReInit, UnprocessableValue, GenericTemplateError, processedError };
